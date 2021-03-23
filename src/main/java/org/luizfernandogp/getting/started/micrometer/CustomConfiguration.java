de548eb09d6f4b8197a08e535fa3ccb1ca31e8af
luiz fernando <luiz.pinto@grupopan.com>
2021-03-23 16:56:45 -0300

    quarkus app - open Api swagger ui and metrics collected and converted to prometheus format.

--- src/main/java/org/luizfernandogp/getting/started/micrometer/CustomConfiguration.java
+++ src/main/java/org/luizfernandogp/getting/started/micrometer/CustomConfiguration.java
@@ -18,27 +18,28 @@ import io.quarkus.micrometer.runtime.MeterFilterConstraint;
 public class CustomConfiguration {
 
     @ConfigProperty(name = "deployment.env")
-    String deploymentEnv;
+    private String deploymentEnv;
 
     @ConfigProperty(name = "appname")
-    String appname;
+    private String appname;
+
+    @ConfigProperty(name = "quarkus.http.port")
+    private String port;
 
     /** Define common tags that apply only to a Prometheus Registry */
     @Produces
     @Singleton
     @MeterFilterConstraint(applyTo = PrometheusMeterRegistry.class)
     public MeterFilter configurePrometheusRegistries() {
-        return MeterFilter.commonTags(Arrays.asList(
-                Tag.of("registry", "prometheus")));
+        return MeterFilter.commonTags(Arrays.asList(Tag.of("registry", "prometheus")));
     }
 
     /** Define common tags that apply globally */
     @Produces
     @Singleton
     public MeterFilter configureAllRegistries() {
-        return MeterFilter.commonTags(Arrays.asList(
-                Tag.of("env", deploymentEnv),
-                Tag.of("application", appname)));
+        return MeterFilter
+                .commonTags(Arrays.asList(Tag.of("application", appname), Tag.of("instance", appname + ":" + port)));
     }
 
     /** Enable histogram buckets for a specific timer */
@@ -48,15 +49,15 @@ public class CustomConfiguration {
         return new MeterFilter() {
             @Override
             public DistributionStatisticConfig configure(Meter.Id id, DistributionStatisticConfig config) {
-                if(id.getName().startsWith("myservice")) {
-                    return DistributionStatisticConfig.builder()
-                        .percentiles(0.5, 0.95)     // median and 95th percentile, not aggregable
-                        .percentilesHistogram(true) // histogram buckets (e.g. prometheus histogram_quantile)
-                        .build()
-                        .merge(config);
+                if (id.getName().startsWith("myservice")) {
+                    return DistributionStatisticConfig.builder().percentiles(0.5, 0.95) // median and 95th percentile,
+                                                                                        // not aggregable
+                            .percentilesHistogram(true) // histogram buckets (e.g. prometheus histogram_quantile)
+                            .build().merge(config);
                 }
                 return config;
             }
         };
     }
+
 }
